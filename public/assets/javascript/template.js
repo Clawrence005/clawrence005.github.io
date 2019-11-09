356465b0f90846409bf217bfcd3fd20f4a3bfffd
colin <clawrence005@yahoo.com>
2019-11-09 01:21:47 -0600

    added copy to clipboard, need to add tool tip

--- public/assets/javascript/template.js
+++ public/assets/javascript/template.js
@@ -12,17 +12,15 @@ function skillsTemplate(skills) {
 document.getElementById("app").innerHTML = `
 <div class="skill-template">${skillsTemplate(skillsArray)}</div>`;
 
-// https://www.geeksforgeeks.org/how-to-copy-the-text-to-the-clipboard-in-javascript/
-function copyContact() {
-  /* Get the text field */
-  var copyContactText = document.getElementById("contactInput");
-
-  /* Select the text field */
-  copyContactText.select();
-
-  /* Copy the text inside the text field */
+// https://stackoverflow.com/questions/31593297/using-execcommand-javascript-to-copy-hidden-text-to-clipboard  #65
+function copyContact(value) {
+  var tempInput = document.createElement("input");
+  tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+  tempInput.value = value;
+  document.body.appendChild(tempInput);
+  tempInput.select();
   document.execCommand("copy");
+  alert("Copied the text: " + tempInput.value);
+  document.body.removeChild(tempInput);
 
-  /* Alert the copied text */
-  alert("Copied the text: " + copyContactText.value);
-}  
+}
